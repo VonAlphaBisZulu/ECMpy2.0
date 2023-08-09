5d33a023b18fea2b2ba3898ef20e06c6803ca58e
A-runaaaa <131651962+A-runaaaa@users.noreply.github.com>
2023-08-09 15:22:04 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -17,7 +17,7 @@ release = '2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+exclude_patterns = ['ECMpy/ECMpy_function.py/NotRequired']
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
